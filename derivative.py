6f5c559e46039e2348ea2c0f83008a78b140c41a
yukun yang <yukunyang@ucsb.edu>
2021-09-17 16:30:59 +0800

    Update derivative.py

--- derivative.py
+++ derivative.py
@@ -71,3 +71,62 @@ plt.plot(u, label='membrane potential')
 plt.plot(a*5, c='red', label='output postsynaptic current')
 plt.legend()
 plt.title('Somatic waveforms')
+
+
+u_e = np.zeros(T)
+s_e = np.zeros(T)
+a_e = np.zeros(T)
+
+s_ei = np.random.rand(N,T)<p
+a_ei = psc(s_ei,tau_s)
+scale = 0.6
+w_e = scale * (np.random.rand(N)-0.5)
+i_e = w_e.dot(a_ei)
+# Forward
+u_e_temp = 0
+for t in range(T):
+    u_e[t] = u_e_temp * (1-1/tau_u) + i[t] + i_e[t]
+    s_e[t] = (u_e[t]>1).astype('float')
+    u_e_temp = u_e[t] * (1-s_e[t])
+a_e = psc(s_e, tau_s)
+
+plt.figure(figsize=(15,5))
+plt.imshow(s_i,cmap='binary')
+plt.title('input spike trains')
+plt.figure(figsize=(15,5))
+plt.imshow(a_i,cmap='binary')
+plt.title('input postsynaptic currents (PSCs)')
+
+plt.figure(figsize=(15,5))
+plt.imshow(s_ei,cmap='binary')
+plt.title('input error spike trains')
+plt.figure(figsize=(15,5))
+plt.imshow(a_ei,cmap='binary')
+plt.title('input error postsynaptic currents (PSCs)')
+
+plt.figure(figsize=(14.8,2))
+plt.plot(i, c='black',label='input PSCs')
+plt.plot(i_e, c='green',label='input error PSCs')
+plt.plot(i+i_e, c='red',label='sum of two')
+plt.legend()
+plt.title('Total input current (= weighted sum of PSCs)')
+
+plt.figure(figsize=(14.8,2))
+plt.plot(s, c='black',label='output spike train')
+plt.plot(u, label='membrane potential')
+plt.plot(a*5, c='red', label='output postsynaptic current')
+plt.legend()
+plt.title('Somatic waveforms')
+
+plt.figure(figsize=(14.8,2))
+plt.plot(s_e, c='black',label='output spike train')
+plt.plot(u_e, label='membrane potential')
+plt.plot(a_e*5, c='red', label='output postsynaptic current')
+plt.legend()
+plt.title('Somatic waveforms with error added')
+
+plt.figure(figsize=(14.8,2))
+plt.plot(i_e, c='green',label='input error PSCs')
+plt.plot((a_e-a), c='red', label='output postsynaptic current difference')
+plt.legend()
+plt.title('error - PSCs difference alignment')
